e35b54e7527bcc3c0836810d9aac7dbb4c7a626f
zhangjun.03 <zhangjun.03@bytedance.com>
2016-05-15 18:01:27 +0800

    举个栗子

--- conf/conf1.js
+++ conf/conf1.js
@@ -1,5 +1,9 @@
 module.exports = {
+  name: 'birds',
   port: 8980,
+  rootPath: __dirname + '/../example1/',
   viewEngine: 'ejs',
+  viewPath: 'views',
+  routerPath: 'controller',
   modules: []
 };
--- core/app.js
+++ core/app.js
@@ -3,40 +3,65 @@ var fs = require('fs');
 var path = require('path');
 var dirname = __dirname;
 var core = {};
-//取得配置文件列表
-function getAppQuene(){
-  return ( fs.readdirSync(dirname + '/../conf') );
-}
+
 //返回 express 实例
 function createExpress(){
   return express();
 }
-//设置模板引擎
-function setViewsEngine(){
-
+//取得某目录文件列表
+function getFileList(){
+  var filePath = arguments[0] || (dirname + '/../conf');
+  return ( fs.readdirSync(filePath) );
 }
-//设置路由
-function setAppRoutes(){
 
+//设置模板引擎
+function setViewsEngine(app){
+  return function(views, viewEngine){
+    app.set('views', views);
+    app.set('view engine', viewEngine);
+  }
+}
+
+//设置路由
+function setAppRoutes(app){
+  return function(routerPath, routerList){
+    console.log(routerList);
+    if(routerList && (routerList instanceof Array)){
+      routerList.forEach(function(item){
+        item = item.replace('.js', '');
+        var router = require(routerPath + item);
+        app.use('/', router);
+      });
+    }else{
+      throw 'arguments error!';
+    }
+  }
+}
+
+function pathParse(){
+  return arguments[0] + arguments[1] + '/';
 }
 //启动所有服务
 function startServer(confList){
   if(confList && (confList instanceof Array))
     confList.forEach(function(item){
       var app = createExpress();
+      var setView = setViewsEngine(app);
+      var setRouter = setAppRoutes(app);
       item = item.replace('.js', '');
       var config = require('../conf/' + item);
-      app.get('/', function(req, res){
-        res.send('hello hello');
-      });
-      console.log(config);
+      setView(pathParse(config.rootPath, config.viewPath), config.viewEngine);
+      setRouter(pathParse(config.rootPath, config.routerPath), getFileList(pathParse(config.rootPath, config.routerPath)));
       config.port && app.listen(config.port);
+
+      console.log(config);
+      console.log(pathParse(config.rootPath, config.routerPath));
       console.log(item + ' start at port ' + config.port);
     });
 }
 
 core.run = function(){
-  startServer(getAppQuene());
+  startServer(getFileList());
 }
 
 module.exports = core;
